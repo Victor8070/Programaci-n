b295b1004060e5a01499201641bf81ca44f912da
Victor8070 <vicmiqest@alu.edu.gva.es>
2025-10-20 12:00:34 +0200

    Add files via upload

---
+++ Ejercicio7.py
@@ -0,0 +1,14 @@
+print("Introduce una nota")
+nota=int(input())
+if nota < 5:
+    print("Insuficiente")
+elif nota < 6:
+    print("Suficiente")
+elif nota < 7:
+    print("Bien")
+elif nota < 8:
+    print("Notable")
+elif nota < 9:
+    print("Sobresaliente")
+elif nota == 10:
+    print("Sobresaliente")
